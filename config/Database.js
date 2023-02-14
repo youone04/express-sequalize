f911d69052498567b230f5a1ddae5d2ae453765a
youone04 <yudi.gunaone87@gmail.com>
2023-02-14 13:13:03 +0700

    ipdate

--- config/Database.js
+++ config/Database.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: process.env.DB_HOST,
     timestamps: false,
     dialect: process.env.BD_DIALECT,
-    port: 19971,
+    port: 10292,
     logging: false,
     dialectOptions: {
       requestTimeout: 30000,
